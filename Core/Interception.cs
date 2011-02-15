cd1a34abe6b4e60f4ccd3c02efbd73989967c870
fir3pho3nixx@gmail.com <fir3pho3nixx@gmail.com>
2011-02-15 09:11:32 +0000

    Added missing file to repository

---
+++ Core/Interception.cs
@@ -0,0 +1,13 @@
+﻿namespace CryoAOP.Core
+{
+    public class Interception
+    {
+        public static void RegisterType(string assemblyPath, string fullTypeName, string outputAssemblyPath = null)
+        {
+            var assemblyInspector = new AssemblyInspector(assemblyPath);
+            TypeInspector typeInspector = assemblyInspector.FindType(fullTypeName);
+            typeInspector.InterceptAllMethods();
+            assemblyInspector.Write(outputAssemblyPath ?? assemblyPath);
+        }
+    }
+}
