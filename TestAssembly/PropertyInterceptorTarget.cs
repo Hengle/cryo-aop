038da89a0fd1befffa9bf52bd7f76e4be9a07552
fir3pho3nixx <fir3pho3nixx@gmail.com>
2011-03-10 08:00:43 +0000

    The beginnings of property interception

---
+++ TestAssembly/PropertyInterceptorTarget.cs
@@ -0,0 +1,12 @@
+﻿namespace CryoAOP.TestAssembly
+{
+    public class PropertyInterceptorTarget
+    {
+        private int someInteger = 0;
+        public int SomeInteger
+        {
+            get { return someInteger; }
+            set { someInteger = value; }
+        }
+    }
+}
