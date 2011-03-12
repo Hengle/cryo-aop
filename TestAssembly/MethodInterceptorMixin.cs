a5fb8ca5c1a7777da89b4660807e88238e792bf7
fir3pho3nixx <fir3pho3nixx@gmail.com>
2011-03-12 21:05:31 +0000

    Added missing type

---
+++ TestAssembly/MethodInterceptorMixin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CryoAOP.Core;
+using CryoAOP.Core.Attributes;
+
+namespace CryoAOP.TestAssembly
+{
+    public class MethodInterceptorMixin
+    {
+        [MixinMethod(typeof(MethodInterceptorTarget))]
+        public void WhenMethodCalled(Action<MethodInvocation> i)
+        {
+            Intercept.Call += i;
+        }
+        
+        [MixinMethod(typeof(MethodInterceptorTarget))]
+        public static void WhenStaticMethodCalled(Action<MethodInvocation> i)
+        {
+            Intercept.Call += i;
+        }
+    }
+}
