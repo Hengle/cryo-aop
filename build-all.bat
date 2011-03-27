8e6bfe236057411e77ec9714da9cf32d70c373ac
fir3pho3nixx <fir3pho3nixx@gmail.com>
2011-03-27 12:25:00 +0100

    Upgraded nunit and added nowarn to build all script

--- build-all.bat
+++ build-all.bat
@@ -1,8 +1,9 @@
+@echo off
 set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319
-msbuild .\CryoAOP.sln /t:Rebuild /p:Configuration=net_2_0_Debug
-msbuild .\CryoAOP.sln /t:Rebuild /p:Configuration=net_2_0_Release
-msbuild .\CryoAOP.sln /t:Rebuild /p:Configuration=net_3_5_Debug
-msbuild .\CryoAOP.sln /t:Rebuild /p:Configuration=net_3_5_Release
-msbuild .\CryoAOP.sln /t:Rebuild /p:Configuration=net_4_0_Debug
-msbuild .\CryoAOP.sln /t:Rebuild /p:Configuration=net_4_0_Release
+msbuild .\CryoAOP.sln /t:Rebuild /p:Configuration=net_2_0_Debug;NoWarn="0649;1685" /verbosity:normal
+msbuild .\CryoAOP.sln /t:Rebuild /p:Configuration=net_2_0_Release;NoWarn="0649;1685" /verbosity:normal
+msbuild .\CryoAOP.sln /t:Rebuild /p:Configuration=net_3_5_Debug;NoWarn=0649 /verbosity:normal
+msbuild .\CryoAOP.sln /t:Rebuild /p:Configuration=net_3_5_Release;NoWarn=0649 /verbosity:normal
+msbuild .\CryoAOP.sln /t:Rebuild /p:Configuration=net_4_0_Debug;NoWarn=0649 /verbosity:normal
+msbuild .\CryoAOP.sln /t:Rebuild /p:Configuration=net_4_0_Release;NoWarn=0649 /verbosity:normal
 pause
